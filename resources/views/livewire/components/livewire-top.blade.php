1af65bcd16c69bad818a381c2b438228a221f3cb
akaOne <desouzakevinm@gmail.com>
2020-12-18 13:02:07 +0000

    Theme icone when not set

--- resources/views/livewire/components/livewire-top.blade.php
+++ resources/views/livewire/components/livewire-top.blade.php
@@ -1,5 +1,5 @@
 <div x-cloak x-data="TOP_DATA()" class="border-b py-2 px-4 select-none flex justify-between items-center">
-    <x-heroicon-s-light-bulb x-on:click="setTheme('light')" x-show="currentTheme == 'dark'" class="w-6 h-6 text-gray-400"/>
+    <x-heroicon-s-light-bulb x-on:click="setTheme('light')" x-show="currentTheme == 'dark' || !localStorage.theme" class="w-6 h-6 text-gray-400"/>
     <x-heroicon-s-moon x-on:click="setTheme('dark')" x-show="currentTheme == 'light'" class="w-6 h-6 text-gray-400"/>
     <span class="font-light">{{ $user->name }}</span>
 </div>
